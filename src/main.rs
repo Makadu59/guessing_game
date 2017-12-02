c8023f581e2c4cbc27f8944d0f88c756959ec29a
Karine Durand-Garçon <alizee59@gmail.com>
2017-12-02 21:24:25 +0100

    saisie et restitution d'une valeur

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// use a standard prelude library
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!"); // prompt "guess the number"
+
+    println!("Please input your guess."); // prompt "Please input your guess"
+
+    let mut guess = String::new(); // initialize a variable named guess
+
+// call a handle from the io library and 2 methods, the second one is the panic! to handle the error
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
