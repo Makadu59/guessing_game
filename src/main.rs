b6ae2430258f8c36c34ec436ecddd60977021750
Karine Durand-Garçon <alizee59@gmail.com>
2017-12-04 14:58:40 +0100

    generating a secret number

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+// use extern crate
+extern crate rand;
+
 // use a standard prelude library
 use std::io;
+// use Rng
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!"); // prompt "guess the number"
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess."); // prompt "Please input your guess"
 
