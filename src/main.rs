d43729c2ad2e5824e152417bf469e289fcae09ee
Karine Durand-Garçon <alizee59@gmail.com>
2017-12-05 13:30:48 +0100

    comparing guesses

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ extern crate rand;
 
 // use a standard prelude library
 use std::io;
+use std::cmp::Ordering;
 // use Rng
 use rand::Rng;
+
 
 fn main() {
     println!("Guess the number!"); // prompt "guess the number"
@@ -21,5 +23,14 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+    Ordering::Less    => println!("Too small!"),
+    Ordering::Greater => println!("Too big!"),
+    Ordering::Equal   => println!("You win!"),
+  }
 }
