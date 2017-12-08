df3cac1ef6bd66406b5d64c5626809832d1f969f
Karine Durand-Garçon <alizee59@gmail.com>
2017-12-08 20:34:25 +0100

    fin du projet

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,41 @@ extern crate rand;
 // use a standard prelude library
 use std::io;
 use std::cmp::Ordering;
-// use Rng
 use rand::Rng;
 
 
 fn main() {
     println!("Guess the number!"); // prompt "guess the number"
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1, 101); // note to myself : je comprends toujours pas cette notation
 
-    println!("The secret number is: {}", secret_number);
+   // println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess."); // prompt "Please input your guess"
+    loop {
+      let mut guess = String::new(); 
 
-    let mut guess = String::new(); // initialize a variable named guess
-
-// call a handle from the io library and 2 methods, the second one is the panic! to handle the error
-    io::stdin().read_line(&mut guess)
+      // call a handle from the io library and 2 methods, the second one is the panic! to handle the error
+      io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+     // let guess: u32 = guess.trim() .parse()
+      //  .expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+      let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+      };
 
-    match guess.cmp(&secret_number) {
-    Ordering::Less    => println!("Too small!"),
-    Ordering::Greater => println!("Too big!"),
-    Ordering::Equal   => println!("You win!"),
+      println!("You guessed: {}", guess);
+
+      match guess.cmp(&secret_number) {
+        Ordering::Less    => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal   => {
+          println!("You win!");
+          break;
+        }
+    }
+
   }
 }
